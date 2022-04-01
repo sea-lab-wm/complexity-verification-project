3e12fa94e9e37ab08a261b7b79fd56ea39a8a8ed
Kobi Feldman <kobif2001@gmail.com>
2022-03-31 22:40:27 -0400

    Import already completed classes

--- src/main/java/edu/wm/kobifeldman/Main.java
+++ src/main/java/edu/wm/kobifeldman/Main.java
@@ -1,7 +1,23 @@
 package edu.wm.kobifeldman;
 
+import edu.wm.kobifeldman.fMRI_Study_Classes.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        //@Nullable Object obj = null;  // might be null
+        //@NonNull  Object nnobj = new Object();  // never null
+        //obj.toString();         // checker warning:  dereference might cause null pointer exception
+        //nnobj = obj;           // checker warning:  nnobj may become null
+        //if (nnobj == null){ // checker warning:  redundant test
+
+        //}
+
+        //@NonNull Object ref = null;
+
+        //@Nullable Boolean test = null;
+
+        ArrayAverage.main();
+        ContainsSubstring.main();
+        YesNo.main();
     }
 }
