9fe55a35885ddefced1f78b46e2ae4ac866db3a9
Kobi Feldman <kobif2001@gmail.com>
2022-04-12 22:22:58 -0400

    Update run.bat
    Now runs the parsing script automatically after running the checker tool

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 call gradlew build --rerun-tasks 2> output.txt
-call java -jar build\libs\complexity-verification-project.jar
+::call java -jar build\libs\complexity-verification-project.jar
+call complexity_verification_project_venv\Scripts\activate.bat
+call complexity_verification_project_venv\Scripts\python.exe parser.py
 pause
