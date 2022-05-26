f41b10866fe55363edf6488925109661766636f9
Kobi Feldman <kobif2001@gmail.com>
2022-05-26 10:42:53 -0400

    Fix parser.py

--- parser.py
+++ parser.py
@@ -3,18 +3,22 @@ import pandas as pd
 # Retrieves the snippet name and warning message for each warning output by the Checker Framework
 def parseCheckerFramework(data):
     lines = []
-    with open('output.txt') as f:
+    with open('checker_framework_output.txt') as f:
         lines = f.readlines()
 
     # Delimeters with which to parse the warnings
-    startSnippet = "\\fMRI_Study_Classes\\"
-    endSnippet = ".java"
-    startError = "] "
+    startSnippetfMRI = "\\fMRI_Study_Classes\\"
+    startSnippetCOG = "\\cog_complexity_validation_datasets\\"
+    endSnippet = ": warning:"
+    #startError = " ["
 
     for line in lines:
-        if startSnippet in line:
-            data["Snippet"].append((line.split(startSnippet))[1].split(endSnippet)[0])
-            data["Error Type"].append(line.split(startError)[1].strip())
+        if startSnippetfMRI in line and endSnippet in line:
+            data["Snippet"].append(startSnippetfMRI + ": " + (line.split(startSnippetfMRI))[1].split(endSnippet)[0])
+            data["Error Type"].append(line.split(endSnippet)[1].strip())
+        elif startSnippetCOG in line and endSnippet in line:
+            data["Snippet"].append(startSnippetCOG + ": " + (line.split(startSnippetCOG))[1].split(endSnippet)[0])
+            data["Error Type"].append(line.split(endSnippet)[1].strip())
 
     return data
 
@@ -31,7 +35,7 @@ def setupExcelSheet():
     df["Error Type Copy"] = df["Error Type"]    # Must copy values to column with different name to use same column for "values" and "columns" when making a pivot table
     df = df.pivot_table(values="Error Type", index="Snippet", columns="Error Type Copy", aggfunc="count")
 
-    df.to_excel('demo.xlsx', engine='xlsxwriter')
+    df.to_excel('checker_framework_data.xlsx', engine='xlsxwriter')
 
 if __name__ == '__main__':
     setupExcelSheet()
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-call gradlew build --rerun-tasks 2> output.txt
+call gradlew build --rerun-tasks 2> checker_framework_output.txt
 ::call java -jar build\libs\complexity-verification-project.jar
 call complexity_verification_project_venv\Scripts\activate.bat
 call complexity_verification_project_venv\Scripts\python.exe parser.py
