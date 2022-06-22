6e6072486334facd86631ca996699eee7884d38b
Kobi Feldman <jcfeldman@email.wm.edu>
2022-06-22 12:36:34 -0400

    Reordered snippets in cog dataset 1

--- src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/One/Tasks.java
+++ src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/One/Tasks.java
@@ -8,9 +8,9 @@ import java.util.Arrays;
  */
 public class Tasks {
 
-    // Tasks for fMRI-Setting
+    /*REORDERED THE METHODS*/
 
-    // 1. Faculty
+    // 1. Faculty                                           /*Tasks for fMRI-Setting*/
     public static void main1(String[] args) {
         int result = 1;
         int x = 4;
@@ -22,155 +22,7 @@ public class Tasks {
         System.out.println(result);
     }
 
-    // 7. Find max in list of numbers
-    public static void main7 (String[] args) {
-        int array[] = {2, 19, 5, 17};
-        int result = array[0];
-        for (int i = 1; i < array.length; i++)
-            if (array[i] > result)
-                result = array[i];
-        System.out.println(result);
-    }
-
-    // 8. Cross sum
-    public static void main8(String[] args) {
-        int number = 323;
-        int result = 0;
-
-        while (number!= 0) {
-            result = result + number % 10;
-            number = number / 10;
-        }
-        System.out.println(result);
-    }
-
-    // 9. Prime test
-    public static void main9(String[] args){
-        int number = 11;
-        boolean result = true;
-        for(int i = 2; i < number; i++) {
-            if(number % i == 0) {
-                result = false;
-                break;
-            }
-        }
-        System.out.println(result);
-    }
-
-    // 10. Find middle number of three numbers
-    public static void main10(String[] args) {
-        int num1 = 5;
-        int num2 = 3;
-        int num3 = 10;
-
-        if (num1 > num2 && num1 > num3)
-            System.out.println(num1);
-        else if (num2 > num1 && num2 > num3)
-            System.out.println(num2);
-        else if (num3 > num1 && num3 > num2)
-            System.out.println(num3);
-    }
-
-    // 11. Power
-    public static void main11(String[] args) {
-        int num1 = 2;
-        int num2 = 3;
-        int result = num1;
-        for (int i = 1; i < num2; i++) {
-            result = result * num1;
-        }
-        System.out.println(result);
-    }
-
-    // 13. Swap
-    public static void main13(String[] args) {
-        int var1 = 23;
-        int var2 = 42;
-        int temp;
-        temp = var1;
-        var1 = var2;
-        var2 = temp;
-        System.out.println(var1);
-    }
-
-    // 14. Reverse string
-    public static void main14(String[] args) {
-        String word = "Hello";
-        String result = new String();
-
-        for ( int j = word.length() - 1; j >= 0; j-- )
-            result += word.charAt(j);
-        
-        System.out.println(word);
-    }
-
-    // 17. Check whether substring is contained
-    public static void main17(String[] args) {
-        String word = "Programming in Java";
-        String key1 = "Java";
-        String key2 = "Pascal";
-
-        int index1 = word.indexOf(key1);
-        int index2 = word.indexOf(key2);
-
-        if (index1 != -1)
-            System.out.println("Substring is contained: " + key1);
-        else
-            System.out.println("Substring is not contained: " + key1);
-        if (index2 != -1)
-            System.out.println("Substring is contained: " + key2);
-        else
-            System.out.println("Substring is not contained: " + key2);
-    }
-
-    // 20. Decimal to binary
-    public static void main20(String[] args) {
-        int i=14;
-        String result="";
-
-        while (i>0) {
-            if (i%2 ==0)
-                result="0"+result;
-            else
-                result="1"+result;
-                i=i/2;
-        }
-
-        System.out.println(result); }
-    
-    // 21. Reverse entries of array
-    public static void main21(String[] args) {
-        int[] array = { 1, 6, 4, 10, 2 };
-
-        for (int i = 0; i <= array.length/2-1; i++){
-            int tmp=array[array.length-i-1];
-            array[array.length-i-1] = array[i];
-            array[i]=tmp;
-        }
-
-        for (int i = 0; i <= array.length - 1; i++) { // Note a "{" had to be added here to allow compilation
-            System.out.println(array[i]);
-        }
-    }
-
-    // 22. Median on sorted data
-    public static void main22(String[] args) {
-        int[] array={1,2,4,5,6,10};
-
-        Arrays.sort(array); // Note: this line had to me changed to allow compilation
-
-        float b;
-        if (array.length % 2==1)
-            b=array[array.length /2];
-        else
-            b=(array[array.length/2-1]+array[array.length/2])/2f;
-        
-        System.out.println(b);
-    }
-
-    // First and second pilot study
-
-    // 2. Count same chars at same positions in String
+    // 2. Count same chars at same positions in String              /*First and second pilot study*/
     public static void main2(String[] args) {
         String string1 = "Magdeburg";
         String string2 = "Hamburg";
@@ -190,43 +42,7 @@ public class Tasks {
         System.out.println(counter);
     }
 
-    // 6. Sum from 1 to n
-    public static void main6 (String[] args) {
-        int n = 4; // Note: a ";" had to be added here to allow compilation
-        int result = 0;
-        for (int i = 1; i <= n; i++)
-            result = result + i;
-        System.out.println(result);
-    }
-
-    // 12. Check palindrom
-    public static void main12(String[] args) {
-        String word = "otto";
-        boolean result = true;
-        for (int i = 0, j = word.length() - 1; i < word.length()/2; i++, // Note: "int" before j had to be removed to allow compilation
-            j--) {
-            if (word.charAt(i) != word.charAt(j)) {
-                result = false;
-                break;
-            }
-        }
-        System.out.println(result);
-    }
-
-    // 23. Double entries of array
-    public static void main23(String[] args) {
-        int[] array = { 1, 3, 11, 7, 4 };
-
-        for (int i = 0; i < array.length; i++)
-            array[i] = array[i] * 2;
-
-        for (int i = 0; i <= array.length - 1; i++)
-        System.out.println(array[i]);
-    }
-
-    // Only in the first pilot study
-
-    // 3. Greatest common divisor
+    // 3. Greatest common divisor                                               /*Only in the first pilot study*/
     // Note: To allow compilation, number1 and number2 had to be defined
     public static void main(int number1, int number2) {
         int temp; // Note: a ";" had to be added here to allow compilation
@@ -245,7 +61,7 @@ public class Tasks {
         System.out.println(number2);
     }
 
-    // 4. BubbleSort
+    // 4. BubbleSort                                                        /*Only in the first pilot study*/
     public static void main4(String[] args) {
         int array[] = {14,5,7};
         for (int counter1 = 0; counter1 < array.length; counter1++) {
@@ -262,7 +78,7 @@ public class Tasks {
             System.out.println(array[counter3]);
     }
 
-    // 5. Binary search
+    // 5. Binary search                                     /*Only in the first pilot study*/
     public static void main5(String[] args) {
         int array[] = { 2, 4, 5, 6, 8, 10, 13 };
         int key = 5;
@@ -281,7 +97,112 @@ public class Tasks {
         }
     }
 
-    // 15. Matrix multiplication
+    // 6. Sum from 1 to n                                                          /*First and second pilot study*/
+    public static void main6 (String[] args) {
+        int n = 4; // Note: a ";" had to be added here to allow compilation
+        int result = 0;
+        for (int i = 1; i <= n; i++)
+            result = result + i;
+        System.out.println(result);
+    }
+
+    // 7. Find max in list of numbers                                           /*Tasks for fMRI-Setting*/
+    public static void main7 (String[] args) {
+        int array[] = {2, 19, 5, 17};
+        int result = array[0];
+        for (int i = 1; i < array.length; i++)
+            if (array[i] > result)
+                result = array[i];
+        System.out.println(result);
+    }
+
+    // 8. Cross sum                                                         /*Tasks for fMRI-Setting*/
+    public static void main8(String[] args) {
+        int number = 323;
+        int result = 0;
+
+        while (number!= 0) {
+            result = result + number % 10;
+            number = number / 10;
+        }
+        System.out.println(result);
+    }
+
+    // 9. Prime test                                                       /*Tasks for fMRI-Setting*/
+    public static void main9(String[] args){
+        int number = 11;
+        boolean result = true;
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0) {
+                result = false;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    // 10. Find middle number of three numbers                                      /*Tasks for fMRI-Setting*/
+    public static void main10(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int num3 = 10;
+
+        if (num1 > num2 && num1 > num3)
+            System.out.println(num1);
+        else if (num2 > num1 && num2 > num3)
+            System.out.println(num2);
+        else if (num3 > num1 && num3 > num2)
+            System.out.println(num3);
+    }
+
+    // 11. Power                                                                /*Tasks for fMRI-Setting*/
+    public static void main11(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int result = num1;
+        for (int i = 1; i < num2; i++) {
+            result = result * num1;
+        }
+        System.out.println(result);
+    }
+
+    // 12. Check palindrom                                          /*First and second pilot study*/
+    public static void main12(String[] args) {
+        String word = "otto";
+        boolean result = true;
+        for (int i = 0, j = word.length() - 1; i < word.length()/2; i++, // Note: "int" before j had to be removed to allow compilation
+            j--) {
+            if (word.charAt(i) != word.charAt(j)) {
+                result = false;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    // 13. Swap                                                             /*Tasks for fMRI-Setting*/
+    public static void main13(String[] args) {
+        int var1 = 23;
+        int var2 = 42;
+        int temp;
+        temp = var1;
+        var1 = var2;
+        var2 = temp;
+        System.out.println(var1);
+    }
+
+    // 14. Reverse string                                                       /*Tasks for fMRI-Setting*/
+    public static void main14(String[] args) {
+        String word = "Hello";
+        String result = new String();
+
+        for ( int j = word.length() - 1; j >= 0; j-- )
+            result += word.charAt(j);
+        
+        System.out.println(word);
+    }
+
+    // 15. Matrix multiplication                                    /*Only in the first pilot study*/
     public static void main15(String[] args) {
         int array[][] = {{5,6,7},{4,8,9}};
         int array1[][] = {{6,4},{5,7},{1,1}};
@@ -305,7 +226,7 @@ public class Tasks {
         }
     }
 
-    // 16. Arithmetic mean
+    // 16. Arithmetic mean                                  /*Only in the first pilot study*/
     public static void main16(String[] args) {
         int a = 4;
         int b = 8;
@@ -313,7 +234,26 @@ public class Tasks {
         System.out.println(result);
     }
 
-    // 18. Least common multiple
+    // 17. Check whether substring is contained                                             /*Tasks for fMRI-Setting*/
+    public static void main17(String[] args) {
+        String word = "Programming in Java";
+        String key1 = "Java";
+        String key2 = "Pascal";
+
+        int index1 = word.indexOf(key1);
+        int index2 = word.indexOf(key2);
+
+        if (index1 != -1)
+            System.out.println("Substring is contained: " + key1);
+        else
+            System.out.println("Substring is not contained: " + key1);
+        if (index2 != -1)
+            System.out.println("Substring is contained: " + key2);
+        else
+            System.out.println("Substring is not contained: " + key2);
+    }
+
+    // 18. Least common multiple                                /*Only in the first pilot study*/
     public static void main18(String[] args) {
         int number1 = 23;
         int number2 = 42;
@@ -337,7 +277,7 @@ public class Tasks {
             System.out.println("Error!");
     }
 
-    // 19. Capitalize first letter of word
+    // 19. Capitalize first letter of word                          /*Only in the first pilot study*/
     public static void main(String[] args) {
         String s = "here are a bunch of words";
 
@@ -350,5 +290,61 @@ public class Tasks {
                 .append(words[i].substring(1)); // Note: a ")" had to be added here to allow compilation
         }
         System.out.println(result);
+    }
+
+    // 20. Decimal to binary                                                                /*Tasks for fMRI-Setting*/
+    public static void main20(String[] args) {
+        int i=14;
+        String result="";
+
+        while (i>0) {
+            if (i%2 ==0)
+                result="0"+result;
+            else
+                result="1"+result;
+                i=i/2;
+        }
+
+        System.out.println(result); }
+    
+    // 21. Reverse entries of array                                                 /*Tasks for fMRI-Setting*/
+    public static void main21(String[] args) {
+        int[] array = { 1, 6, 4, 10, 2 };
+
+        for (int i = 0; i <= array.length/2-1; i++){
+            int tmp=array[array.length-i-1];
+            array[array.length-i-1] = array[i];
+            array[i]=tmp;
+        }
+
+        for (int i = 0; i <= array.length - 1; i++) { // Note a "{" had to be added here to allow compilation
+            System.out.println(array[i]);
+        }
+    }
+
+    // 22. Median on sorted data                                                /*Tasks for fMRI-Setting*/
+    public static void main22(String[] args) {
+        int[] array={1,2,4,5,6,10};
+
+        Arrays.sort(array); // Note: this line had to me changed to allow compilation
+
+        float b;
+        if (array.length % 2==1)
+            b=array[array.length /2];
+        else
+            b=(array[array.length/2-1]+array[array.length/2])/2f;
+        
+        System.out.println(b);
+    }
+
+    // 23. Double entries of array                              /*First and second pilot study*/
+    public static void main23(String[] args) {
+        int[] array = { 1, 3, 11, 7, 4 };
+
+        for (int i = 0; i < array.length; i++)
+            array[i] = array[i] * 2;
+
+        for (int i = 0; i <= array.length - 1; i++)
+        System.out.println(array[i]);
     }
 }
