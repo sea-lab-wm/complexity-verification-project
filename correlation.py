fcbb3f15be3c2062286804957c8443f2e3e9cf36
Kobi Feldman <jcfeldman@email.wm.edu>
2022-06-22 18:19:05 -0400

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read all the data output excel sheets from the various analysis tool 
-# and create a set of all unique snippets across all the datasets that contain warnings
+# and create a set of all the unique snippets across all the datasets that contain warnings
 def getSnippetsWithWarnings():
     df = pd.read_excel('checker_framework_data.xlsx', usecols=['Snippet'])
     uniqueSnippets = []
@@ -17,9 +17,20 @@ def getDatasets():
 
     return df['Complexity Metric'].to_list()
 
+# Gets a count of the number of snippets that contain warnings for each dataset
 def sortUniqueSnippetsByDataset(datasets, uniqueSnippets):
+    countSnippetsPerDataset = dict([(key.split("-")[1].strip(), 0) for key in datasets])
+ 
+    for snippet in uniqueSnippets:
+        snippet = snippet.split("-")[0].strip()
+
+        if snippet in countSnippetsPerDataset:
+            countSnippetsPerDataset[snippet] += 1
+
+    return countSnippetsPerDataset
+
+def setNumSnippetsWithWarningsColumn(countSnippetsPerDataset):
     pass
 
 if __name__ == '__main__':
-    getSnippetsWithWarnings()
-    getDatasets()
+    sortUniqueSnippetsByDataset(getDatasets(), getSnippetsWithWarnings())
