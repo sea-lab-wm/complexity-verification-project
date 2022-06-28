4ca7e54bbb686529f2ec684689112190f2115f42
Kobi Feldman <kobif2001@gmail.com>
2022-06-28 11:40:46 -0400

    Move typestate checker files

--- src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/Three/testtypestate.bat
+++
@@ -1,2 +0,0 @@
-call java -jar ../../../../../../../../checker-framework-3.14.0/checker/dist/checker.jar -classpath ../../../../../../../../jatyc.jar -processor jatyc.JavaTypestateChecker *.java -Awarns -Xmaxwarns 10000 2> ../../../../../../../../data/typestate_checker_output.txt
-pause
---
+++ tool-execution/run_typestate.bat
@@ -0,0 +1,5 @@
+::call java -jar ../../../../../../../../checker-framework-3.14.0/checker/dist/checker.jar -classpath ../../../../../../../../jatyc.jar -processor jatyc.JavaTypestateChecker *.java -Awarns -Xmaxwarns 10000 2> ../../../../../../../../data/typestate_checker_output.txt
+::pause
+
+call java -jar ../checker-framework-3.14.0/checker/dist/checker.jar -classpath ../jatyc.jar -processor jatyc.JavaTypestateChecker ../src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/Three/*.java -Awarns -Xmaxwarns 10000 2> ../data/typestate_checker_output.txt
+pause
