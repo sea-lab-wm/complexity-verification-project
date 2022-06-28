0cd4f06157d11089e1aea5ae6606b69780392a81
Kobi Feldman <kobif2001@gmail.com>
2022-06-28 18:45:19 -0400

    Alter structure of typestate checker execution again

--- tool-execution/run_typestate_checker.bat
+++ tool-execution/run_typestate_checker.bat
@@ -1,2 +1,3 @@
-call typestate_checker_cog_dataset_3.bat
+call java -jar ../checker-framework-3.14.0/checker/dist/checker.jar -classpath ../jatyc.jar -processor jatyc.JavaTypestateChecker ../src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/Three/*.java  -d classes -Awarns -Xmaxwarns 10000 2> ../data/typestate_checker_output_cog_dataset_3.txt
+::TODO: Add more datasets here
 pause
