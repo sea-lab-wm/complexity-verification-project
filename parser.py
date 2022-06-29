f4b0fd54fca187012989a50656e231d44deff735
Kobi Feldman <kobif2001@gmail.com>
2022-06-29 18:19:02 -0400

    Update parser.py
    Restructured to handle multiple analysis tools.

--- parser.py
+++ parser.py
@@ -9,7 +9,11 @@ import pandas as pd
 cog_dataset_1_methods_lines = [14, 26, 47, 65, 82, 101, 110, 120, 132, 145, 159, 170, 184, 195, 206, 230, 238, 257, 281, 296, 311, 326, 341]
 cog_dataset_3_methods_lines = [212, 228, 245, 277, 289, 298, 314, 322, 332, 345, 361, 371, 384, 395, 409, 417, 425, 439, 451, 460, 468, 483, 492, 502, 513, 524, 538, 547, 558, 571, 580, 601, 614, 621, 638, 654, 666, 678, 689, 704, 714, 725, 736, 745, 753, 766, 781, 788, 803, 820, 832, 842, 852, 866, 876, 894, 901, 909, 917, 927, 934, 946, 957, 967, 976, 985, 1000, 1014, 1022, 1032, 1044, 1056, 1077, 1089, 1106, 1115, 1123, 1133, 1141, 1158, 1172, 1185, 1202, 1213, 1228, 1238, 1251, 1262, 1273, 1290, 1305, 1322, 1336, 1351, 1362, 1375, 1383, 1398, 1406, 1421, 1431]
 
-# Retrieves the snippet name and warning message for each warning output by the Checker Framework
+##################################
+#   Parse Analysis Tool Output   #
+##################################
+
+# Retrieves the snippet name and warning message for each warning output by the Checker Framework.
 def parseCheckerFramework(data):
     lines = []
     with open('data/checker_framework_output.txt') as f:
@@ -42,23 +46,36 @@ def parseCheckerFramework(data):
                     data["Warning Type"].append(line.split(endSnippet)[1].strip())
                     break
     
-    #print(str(len(data["Snippet"])) + str(len(data["Warning Type"])))
-    return data
+    return ('checker_framework_data', data)
 
-def setupExcelSheet():
+def parseTypestateChecker(data):
+    pass
+
+########################
+#   Setup CSV Sheets   #
+########################
+
+# Creates csv files for each analysis tool's warning output.
+def setupCSVSheets(allAnalysisToolData):
+    for data in allAnalysisToolData:
+        # Creates a table from the dictionary of data
+        df = pd.DataFrame(parseCheckerFramework(data[1]))
+
+        # Convert data using pivot table
+        df["Warning Type Copy"] = df["Warning Type"]    # Must copy values to column with different name to use same column for "values" and "columns" when making a pivot table
+        df = df.pivot_table(values="Warning Type", index="Snippet", columns="Warning Type Copy", aggfunc="count")
+
+        df.to_csv(f'data/{data[0]}.csv')
+
+if __name__ == '__main__':
     # Data output structure as a dictionary
     data = {
         "Snippet": [],
         "Warning Type": []
     }
+    allAnalysisToolData = []
 
-    # Creates a table from the dictionary of data
-    df = pd.DataFrame(parseCheckerFramework(data))
+    #TODO: All analysis tools go here
+    allAnalysisToolData.append(parseCheckerFramework(data))
 
-    df["Warning Type Copy"] = df["Warning Type"]    # Must copy values to column with different name to use same column for "values" and "columns" when making a pivot table
-    df = df.pivot_table(values="Warning Type", index="Snippet", columns="Warning Type Copy", aggfunc="count")
-
-    df.to_csv('data/checker_framework_data.csv')
-
-if __name__ == '__main__':
-    setupExcelSheet()
+    setupCSVSheets(allAnalysisToolData)
