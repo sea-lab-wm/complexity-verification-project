3228c7cfb688aa08cf8567926743468664b7dded
Kobi Feldman <kobif2001@gmail.com>
2022-07-01 15:12:11 -0400

    Update parser.py
    Now determines the method names from line numbers in a much better way than before.

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 # OSCAR: how are the tools run?
 
@@ -6,15 +7,30 @@ import pandas as pd
 # OSCAR: why do we need these line numbers? my understanding is that the codes checks that the warnings fall within
 # these numbers. Does it mean that the warning output file contains extra warnings we don't want to parse? I thought we would parse all the warnings in the file.
 # OSCAR: update, I checked the correlation code, and I think the reason for these lines is that we need to identify the snippets for which we get warnings (i.e., get the snippet ID)
-cog_dataset_1_methods_lines = [14, 26, 47, 65, 82, 101, 110, 120, 132, 145, 159, 170, 184, 195, 206, 230, 238, 257, 281, 296, 311, 326, 341]
-cog_dataset_3_methods_lines = [212, 228, 245, 277, 289, 298, 314, 322, 332, 345, 361, 371, 384, 395, 409, 417, 425, 439, 451, 460, 468, 483, 492, 502, 513, 524, 538, 547, 558, 571, 580, 601, 614, 621, 638, 654, 666, 678, 689, 704, 714, 725, 736, 745, 753, 766, 781, 788, 803, 820, 832, 842, 852, 866, 876, 894, 901, 909, 917, 927, 934, 946, 957, 967, 976, 985, 1000, 1014, 1022, 1032, 1044, 1056, 1077, 1089, 1106, 1115, 1123, 1133, 1141, 1158, 1172, 1185, 1202, 1213, 1228, 1238, 1251, 1262, 1273, 1290, 1305, 1322, 1336, 1351, 1362, 1375, 1383, 1398, 1406, 1421, 1431]
+#cog_dataset_1_methods_lines = [14, 26, 47, 65, 82, 101, 110, 120, 132, 145, 159, 170, 184, 195, 206, 230, 238, 257, 281, 296, 311, 326, 341]
+#cog_dataset_3_methods_lines = [212, 228, 245, 277, 289, 298, 314, 322, 332, 345, 361, 371, 384, 395, 409, 417, 425, 439, 451, 460, 468, 483, 492, 502, 513, 524, 538, 547, 558, 571, 580, 601, 614, 621, 638, 654, 666, 678, 689, 704, 714, 725, 736, 745, 753, 766, 781, 788, 803, 820, 832, 842, 852, 866, 876, 894, 901, 909, 917, 927, 934, 946, 957, 967, 976, 985, 1000, 1014, 1022, 1032, 1044, 1056, 1077, 1089, 1106, 1115, 1123, 1133, 1141, 1158, 1172, 1185, 1202, 1213, 1228, 1238, 1251, 1262, 1273, 1290, 1305, 1322, 1336, 1351, 1362, 1375, 1383, 1398, 1406, 1421, 1431]
+
+#########################
+#   Get Snippet Names   #
+#########################
+
+# Parses a .java file containing numbered snippets to determine which snippet starts on which line.
+def getSnippetNames(filePath):
+    methodStartLines = []
+
+    with open(filePath) as f:
+        for lineNum, line in enumerate(f, start=1):
+            if 'SNIPPET_STARTS' in line:
+                methodStartLines.append(lineNum + 1)
+
+    return methodStartLines
 
 ##################################
 #   Parse Analysis Tool Output   #
 ##################################
 
 # Retrieves the snippet name and warning message for each warning output by the Checker Framework.
-def parseCheckerFramework(data):
+def parseCheckerFramework(data, fMRIDatasetSnippetNames, cogDataset1SnippetNums, cogDataset3SnippetNums):
     lines = []
     with open('data/checker_framework_output.txt') as f:
         lines = f.readlines()
@@ -27,28 +43,31 @@ def parseCheckerFramework(data):
 
     for line in lines:
         if startSnippetfMRI in line and endSnippet in line:
-            data["Snippet"].append("fMRI Dataset - " + (line.split(startSnippetfMRI))[1].split(".java")[0])
+            data["Snippet"].append("fMRI Dataset - " + str(fMRIDatasetSnippetNames.index((line.split(startSnippetfMRI))[1].split(".java")[0]) + 1))
             data["Warning Type"].append(line.split(endSnippet)[1].strip())
         elif startSnippetCOG1 in line and endSnippet in line:
             lineNum = int(line.split(".java:")[1].split(":")[0])
 
-            for i in range(len(cog_dataset_1_methods_lines) - 1):
-                if cog_dataset_1_methods_lines[i] < lineNum and cog_dataset_1_methods_lines[i + 1] > lineNum:
+            for i in range(len(cogDataset1SnippetNums) - 1):
+                if cogDataset1SnippetNums[i] < lineNum and cogDataset1SnippetNums[i + 1] >= lineNum:
                     data["Snippet"].append("COG Dataset 1 - " + str(i + 1))
                     data["Warning Type"].append(line.split(endSnippet)[1].strip())
                     break
         elif startSnippetCOG3 in line and endSnippet in line:
             lineNum = int(line.split(".java:")[1].split(":")[0])
 
-            for i in range(len(cog_dataset_3_methods_lines) - 1):
-                if cog_dataset_3_methods_lines[i] < lineNum and cog_dataset_3_methods_lines[i + 1] > lineNum:
+            for i in range(len(cogDataset3SnippetNums) - 1):
+                if cogDataset3SnippetNums[i] < lineNum and cogDataset3SnippetNums[i + 1] >= lineNum:
                     data["Snippet"].append("COG Dataset 3 - " + str(i + 1))
                     data["Warning Type"].append(line.split(endSnippet)[1].strip())
                     break
     
     return ('checker_framework_data', data)
 
+# Retrieves the snippet name and warning message for each warning output by the Typestate Checker.
 def parseTypestateChecker(data):
+
+
     pass
 
 ########################
@@ -59,7 +78,7 @@ def parseTypestateChecker(data):
 def setupCSVSheets(allAnalysisToolData):
     for data in allAnalysisToolData:
         # Creates a table from the dictionary of data
-        df = pd.DataFrame(parseCheckerFramework(data[1]))
+        df = pd.DataFrame(data[1])
 
         # Convert data using pivot table
         df["Warning Type Copy"] = df["Warning Type"]    # Must copy values to column with different name to use same column for "values" and "columns" when making a pivot table
@@ -68,6 +87,14 @@ def setupCSVSheets(allAnalysisToolData):
         df.to_csv(f'data/{data[0]}.csv')
 
 if __name__ == '__main__':
+    fMRIDatasetSnippetNames = [name.split('.')[0] for name in os.listdir('src/main/java/edu/wm/kobifeldman/fMRI_Study_Classes')]
+    cogDataset1SnippetNums = getSnippetNames('src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/One/Tasks.java')
+    cogDataset3SnippetNums = getSnippetNames('src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/Three/Tasks.java')
+
+    #print(cogDataset3SnippetNums)
+    #for i in range(len(cogDataset3SnippetNums) - 1):
+    #    print(cogDataset3SnippetNums[i])
+
     # Data output structure as a dictionary
     data = {
         "Snippet": [],
@@ -76,6 +103,6 @@ if __name__ == '__main__':
     allAnalysisToolData = []
 
     #TODO: All analysis tools go here
-    allAnalysisToolData.append(parseCheckerFramework(data))
+    allAnalysisToolData.append(parseCheckerFramework(data, fMRIDatasetSnippetNames, cogDataset1SnippetNums, cogDataset3SnippetNums))
 
     setupCSVSheets(allAnalysisToolData)
