572a70e35e830f93924c0945a737a5a439707002
Kobi Feldman <kobif2001@gmail.com>
2022-07-05 13:43:38 -0400

    Add run_typestate_checker.sh

---
+++ tool-execution/run_typestate_checker.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Running the Typestate Checker. This may take some time..."
+
+java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar -processor jatyc.JavaTypestateChecker src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/One/*.java -d classes -Awarns -Xmaxwarns 10000 2> data/typestate_checker_output_cog_dataset_1.txt
+java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar -processor jatyc.JavaTypestateChecker src/main/java/edu/wm/kobifeldman/cog_complexity_validation_datasets/Three/*.java -d classes -Awarns -Xmaxwarns 10000 2> data/typestate_checker_output_cog_dataset_3.txt
+java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar -processor jatyc.JavaTypestateChecker src/main/java/edu/wm/kobifeldman/fMRI_Study_Classes/*.java -d classes -Awarns -Xmaxwarns 10000 2> data/typestate_checker__output_fMRI_dataset.txt
+
+echo "Typestate Checker successfuly ran. View output in data/typestate_checker_output_{dataset name}.txt"
