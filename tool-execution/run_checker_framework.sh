b6bb8bbcf739e53dec06af09e3cb015e04c80940
Kobi Feldman <kobif2001@gmail.com>
2022-07-05 13:43:51 -0400

    Update run_checker_framework.sh

--- tool-execution/run_checker_framework.sh
+++ tool-execution/run_checker_framework.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "Running the Checker Framework. This may take some time..."
+
 ./gradlew build --rerun-tasks 2> data/checker_framework_output.txt
 #java -jar build\libs\complexity-verification-project.jar
 
