6667b4f6da8ae1f55301f7c6e375848d10ac21c2
Kobi Feldman <kobif2001@gmail.com>
2022-07-07 15:54:52 -0400

    Update Main.java

--- src/main/java/edu/wm/kobifeldman/Main.java
+++ src/main/java/edu/wm/kobifeldman/Main.java
@@ -4,19 +4,7 @@ import edu.wm.kobifeldman.fMRI_Study_Classes.*;
 
 public class Main {
     public static void main(String[] args) {
-        //@Nullable Object obj = null;  // might be null
-        //@NonNull  Object nnobj = new Object();  // never null
-        //obj.toString();         // checker warning:  dereference might cause null pointer exception
-        //nnobj = obj;           // checker warning:  nnobj may become null
-        //if (nnobj == null){ // checker warning:  redundant test
-
-        //}
-
-        //@NonNull Object ref = null;
-        //Object ref = null;
-
-        //@Nullable Boolean test = null;
-
+        // fMRI Study Snippets:
         ArrayAverage.main();
         ContainsSubstring.main();
         CountVowels.main();
@@ -33,5 +21,7 @@ public class Main {
         RecursivePower.main();
         SquareRoot.main();
         YesNo.main();
+
+        
     }
 }
