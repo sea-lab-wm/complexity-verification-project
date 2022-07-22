743c9b16e20887df6d900b1b9a4916f190502e30
Kobi Feldman <kobif2001@gmail.com>
2022-07-22 15:12:07 -0400

    Update run_jbmc.sh

--- tool-execution/run_jbmc.sh
+++ tool-execution/run_jbmc.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+if ! command -v jbmc &> /dev/null
+then
+    echo "jmbc could not be found"
+    exit
+fi
+
 ( cd simple-datasets/src/main/java && javac Main.java )
-( cd simple-datasets/src/main/java && jbmc Main --unwind 20 > ../../../../data/jbmc_output.txt )
+( cd simple-datasets/src/main/java && jbmc Main --unwind 5 > ../../../../data/jbmc_output.txt )
