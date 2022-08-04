efd0604a34a1721bbf061e7c788e18048c021924
Oscar Chaparro <ojcch1@gmail.com>
2022-08-04 00:04:40 -0400

    meta-analysis results

---
+++ meta-analysis/meta-analysis.R
@@ -0,0 +1,71 @@
+# NOTE: make sure to set your working directory first with setwd("Path/to/this/directory")
+
+##---------------------------------------------------------------##
+##                Load libraries and import data                 ##
+##---------------------------------------------------------------##
+
+# Load libraries
+library(readxl)
+library(writexl)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(meta)
+
+# Returns pearson's r for the given tau using Kendall's formula (1970)
+convert_to_pearson <- function(tau) {
+  return(sin(0.5 * pi * tau))
+}
+
+
+# Performs a correlation meta analysis on the given data and saves the forestplot to file
+print_meta_analysis <- function(data_in, generateForestPlot, label) {
+  
+  correlation_data = select(subset(data_in, metric_type == label), c('var_name', 
+                                                                 "n",
+                                                                 "cor",
+                                                                 "p_value"))
+  
+  correlation_data$cor <- sapply(correlation_data$cor, convert_to_pearson)
+  meta_analysis_result <- metacor(cor, n, data = correlation_data,
+                                  studlab = correlation_data$var_name,
+                                  sm = "ZCOR", comb.fixed=FALSE,
+                                  method.tau = "SJ")
+  print(meta_analysis_result)
+  if (generateForestPlot) {
+    path = "./forest-plot/"
+    dir.create(path, showWarnings = FALSE) # Create directory if it doesn't exist
+    png(file = paste(path, label, "_forestplot.png", sep = ""), 
+        width = 1535, 
+        height = 575, res = 180)
+    #pdf(file = paste(path, label, "_forestplot.pdf", sep = ""))
+    forest_plot <- forest(meta_analysis_result, 
+                          #prediction = TRUE, 
+                          smlab = "Correlation meta-analysis",
+                          leftlabs = c("Metric", "# of snippets"))
+    dev.off()
+    print(forest_plot)
+  }
+}
+
+
+all_tools_data = read_excel("correlation_analysis.xlsx", sheet = "all_tools")
+
+all_tools_data2 = select(all_tools_data, c('dataset','metric_type',
+                                           'higher_warnings', 
+                                           "# of data points for correlation",
+                                           "Kendall's Tau",
+                                           "Kendall's p-value"))
+colnames(all_tools_data2) <- c('dataset','metric_type',
+                               'higher_warnings', 
+                               "n",
+                               "cor",
+                               "p_value")
+all_tools_data2$var_name = paste(all_tools_data2$dataset, "_", all_tools_data2$metric_type, sep = "")
+
+metric_types = unique(all_tools_data2$metric_type)
+
+print_meta_analysis(all_tools_data2, TRUE, metric_types[1])
+lapply(metric_types, function (label){print_meta_analysis(all_tools_data2, TRUE, label)})
+  
+  
--- meta-analysis/test_meta_analysis.R
+++ meta-analysis/test_meta_analysis.R
@@ -29,6 +29,37 @@ meta_analysis_result_temp <- metacor(cor, n, data = correlation_data,
 #Michael Borenstein, Larry V Hedges, Julian PT Higgins, and Hannah R Rothstein. 2009. Effect Sizes Based on Correlations. In Introduction to meta-analysis. John Wiley & Sons, Chichester, United Kingdom, 40–43. 
 #Michael Borenstein, Larry V Hedges, Julian PT Higgins, and Hannah R Rothstein. 2009. Random-Effects Model. In Introduction to meta-analysis. John Wiley & Sons, Chichester, United Kingdom, 68–75. 
 #Michael Borenstein, Larry V Hedges, Julian PT Higgins, and Hannah R Rothstein. 2009. When does it make sense to perform a meta-analysis. In Introduction to meta-analysis. John Wiley & Sons, Chichester, United Kingdom, 357–64.
+
+#Fixed-Effect Versus Random-Effects Models (Borenstein's book):
+#   If the number of studies is very small, then theestimate of the between-studies variance (\tau^2) will have poor precision.
+
+#The parameter tau-squared (\tau^2) is defined as the variance of the true effect sizes. Inother words, if we had an infinitely large sample of studies, each, itself, infinitelylarge (so that the estimate in each study was the true effect) and computed thevariance of these effects, this variance would be \tau^2.
+
+#The statisticsT2(andT) reflect theamountoftrue heterogeneity (the variance or the standard deviation) whileI2reflects theproportion of observed dispersion that is due to this heterogeneity. In a sense, if wewere to multiply the observed variance byI2, we would getT2(this is meant as anillustration only, since the actual computation is more complicated). As such, thetwo tend to move in tandem, but have very different meanings
+
+#In sum, when the number of studies is small, there are no really good options. As astarting point we would suggest reporting the usual statistics and then explaining thelimitations as clearly as possible. 
+
+#a confidence interval, reflecting the precision with which the effect size has been estimatedin that study.
+
+# Effect sizeOn the plot the summary effect is shown on the bottom line. In this example thesummary risk ratio is 0.85, indicating that the risk of death (or MI) was 15% lowerfor patients assigned to the high dose than for patients assigned to standard dose.The summary effect is nothing more than the weighted mean of the individualeffects.
+
+# we cannot compute regular average because: (1) the sample size of studies is different, 
+# (2) the random effects model indicates that each study has its own true effect size, 
+#so we want to estimate the mean of the distribution of these sizes. 
+#In this scenario, the average is not appropriate because it does not consider 
+#the variance of the effect sizes of the individual studies
+# (3) it does not consider the precisions of the studies. 
+# We want to assign more weight to the more precise studies,
+
+# dispersion of effect sizes 
+
+#kendalls: https://towardsdatascience.com/kendall-rank-correlation-explained-dee01d99c535
+# correlations cannot be averaged: https://medium.com/@jan.seifert/averaging-correlations-part-i-3adab6995042
+
+#Andrew Gilpin (1993) has a nice table of conversions for tau to Pearson r, spearman rho, and common conversions thereof. I think this will suffice for your project.
+#Gilpin, A. (1993). Table for conversion of Kendall's tau to Spearman's rho within the context of measures of magnitude of effect for meta-analysis. Educational and Psychological Measurement, 53, 87-92. doi:10.1177/0013164493053001007
+#JMASM9: Converting Kendall’s Tau For Correlational Or Meta-Analytic Analyses
+
 print(meta_analysis_result_temp)
 
 #---------------
