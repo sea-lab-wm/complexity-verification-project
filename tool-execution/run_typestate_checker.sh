655d3ac2dddbec5c3cfb085cc03c12ff4c10958b
Kobi Feldman <kobif2001@gmail.com>
2022-08-25 17:38:22 -0400

    Update run_typestate_checker.sh to match changes in main

--- tool-execution/run_typestate_checker.sh
+++ tool-execution/run_typestate_checker.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Running the Typestate Checker. This may take some time..."
 
@@ -6,13 +6,13 @@ java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar
 
 java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar -processor jatyc.JavaTypestateChecker simple-datasets/src/main/java/cog_complexity_validation_datasets/Three/*.java -d classes -Awarns -Xmaxwarns 10000 2> data/typestate_checker_output_cog_dataset_3.txt
 
-# the classpath for dataset 6
+# the classpath and supporting variables for dataset 6
 DATASET6CP=$(./gradlew :dataset6:printClasspath -q)
+# these two variables are not really necessary, but makes the command line a little easier to read
+CF314PATH=$(realpath checker-framework-3.14.0/checker/dist/checker.jar)
+OUTFILE6=$(realpath data/typestate_checker_output_cog_dataset_6.txt)
 
-java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar:"${DATASET6CP}" -processor jatyc.JavaTypestateChecker dataset6/src/main/java/*.java -d classes -Awarns -Xmaxwarns 10000 2> data/typestate_checker_output_cog_dataset_6.txt
-java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar:"${DATASET6CP}" -processor jatyc.JavaTypestateChecker dataset6/src/main/java/weka/estimators/*.java -d classes -Awarns -Xmaxwarns 10000 2>> data/typestate_checker_output_cog_dataset_6.txt
-java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar:"${DATASET6CP}" -processor jatyc.JavaTypestateChecker dataset6/src/main/java/weka/gui/beans/*.java -d classes -Awarns -Xmaxwarns 10000 2>> data/typestate_checker_output_cog_dataset_6.txt
-java -jar checker-framework-3.14.0/checker/dist/checker.jar -classpath jatyc.jar:"${DATASET6CP}" -processor jatyc.JavaTypestateChecker dataset6/src/main/java/weka/gui/experiment/*.java -d classes -Awarns -Xmaxwarns 10000 2>> data/typestate_checker_output_cog_dataset_6.txt
+java -jar ${CF314PATH} -classpath jatyc.jar:"${DATASET6CP}" -processor jatyc.JavaTypestateChecker -d classes -Awarns -Xmaxwarns 10000 $(find dataset6/src/main/java -name "*.java") 2> ${OUTFILE6}
 
 # the classpath for dataset 6
 DATASET9CP=$(./gradlew :dataset9:printClasspath -q)
