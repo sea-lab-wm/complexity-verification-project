e56e80b4239d675d4c964787f985cd113036c73c
Kobi Feldman <kobif2001@gmail.com>
2022-11-04 11:30:04 -0400

    Add read_dataset_3_metrics()

--- ml_model/create_ml_table.py
+++ ml_model/create_ml_table.py
@@ -27,6 +27,19 @@ def get_num_warnings(file, tool):
 def get_metrics():
     """Returns a dataframe containing metric data for each dataset."""
 
+    metric_data = []
+
+    metric_data.append(read_dataset_1_metrics())
+    metric_data.append(read_dataset_3_metrics())
+
+    return pd.concat(metric_data)
+
+def read_dataset_1_metrics():
+    """Reads the results of the first pilot study for COG dataset 1. It contains 41 people who looked at 23 snippets.
+    Metrics include time to solve (in sec.), correctness where 0 = completely wrong, 1 = in part correct, 2 = completely correct, and
+    Subjective rating is on a scale of 0 through 4 where 0 = very difficult, 1 = difficult, 2 = medium, 3 = easy, 4 = very easy.
+    """
+
     dict_df = {
         "dataset_id": [],
         "snippet_id": [],
@@ -34,22 +47,8 @@ def get_metrics():
         "metric": [],
         "metric_type": []
     }
-
     data = pd.DataFrame(dict_df)
 
-    for record in read_cog_dataset_1_metrics():
-        df_record = pd.DataFrame(record)
-        data = pd.concat([data, df_record], ignore_index=True, axis=0)  
-
-    return data
-
-def read_cog_dataset_1_metrics():
-    """Reads the results of the first pilot study for COG dataset 1. It contains 41 people who looked at 23 snippets.
-    Metrics include time to solve (in sec.), correctness where 0 = completely wrong, 1 = in part correct, 2 = completely correct, and
-    Subjective rating is on a scale of 0 through 4 where 0 = very difficult, 1 = difficult, 2 = medium, 3 = easy, 4 = very easy.
-    """
-
-    records = []
     cols = []
 
     df = pd.read_excel("data/cog_dataset_1.xlsx")
@@ -61,18 +60,49 @@ def read_cog_dataset_1_metrics():
 
     for rowIndex, row in df_cols.iterrows(): #iterate over rows
         for columnIndex, value in row.items():
-            records.append(
-                {
+            record = {
                     "dataset_id": ["1"],
                     "snippet_id": [columnIndex.split("::")[0]],
                     "person_id": [rowIndex],
                     "metric": [value],
-                    "metric_type": [columnIndex.split("::")[0].lower()]
+                    "metric_type": [columnIndex.split("::")[1].lower()]
                 }
-            )
+            df_record = pd.DataFrame(record)
+            data = pd.concat([data, df_record], ignore_index=True, axis=0)
 
-    print(len(records))
-    return records
+    return data  
+
+def read_dataset_3_metrics():
+    """Reads the results of the cog data set 3 study. It contains 121 people who rated 100 snippets on a scale of 1-5.
+    1 being less readable and 5 being more readable.
+    """
+
+    dict_df = {
+        "dataset_id": [],
+        "snippet_id": [],
+        "person_id": [],
+        "metric": [],
+        "metric_type": []
+    }
+    data = pd.DataFrame(dict_df)
+
+    df = pd.read_csv("data/cog_dataset_3.csv", header=None)
+
+    df_cols = df.iloc[:, 2:102]
+
+    for rowIndex, row in df_cols.iterrows():
+        for columnIndex, value in row.items():
+            record = {
+                    "dataset_id": ["3"],
+                    "snippet_id": [columnIndex],
+                    "person_id": [rowIndex],
+                    "metric": [value],
+                    "metric_type": ["rating"]
+                }
+            df_record = pd.DataFrame(record)
+            data = pd.concat([data, df_record], ignore_index=True, axis=0)
+
+    return data
 
 if __name__ == "__main__":
     warning_data_files = {
@@ -85,9 +115,9 @@ if __name__ == "__main__":
 
     for name, file in warning_data_files.items():
         warning_data.append(get_num_warnings(file, name))
-        
+
     # Add a set of data where all tools are combined
     warning_data.append(pd.concat(warning_data))
 
-    data = get_metrics()
-    print(data)
+    metric_data = get_metrics()
+    print(metric_data)
