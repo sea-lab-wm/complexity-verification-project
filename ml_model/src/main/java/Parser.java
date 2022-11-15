915ef0ffcdbdbcbe43cf8fc0b6d97e99d4003ead
Kobi Feldman <kobif2001@gmail.com>
2022-11-15 07:07:44 -0500

    Parser.py update

--- ml_model/src/main/java/Parser.java
+++ ml_model/src/main/java/Parser.java
@@ -1,8 +1,10 @@
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.CompilationUnit;
+
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.ConditionalExpr;
 
 import java.io.FileInputStream;
 import java.util.ArrayList;
@@ -15,22 +17,17 @@ public class Parser {
     public static void main(String[] args) throws Exception {
         CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(FILE_PATH));
 
-        VoidVisitor<?> methodNameVisitor = new MethodNamePrinter();
-        methodNameVisitor.visit(cu, null);
-
         List<String> methodNames = new ArrayList<>();
         VoidVisitor<List<String>> methodNameCollector = new MethodNameCollector();
         methodNameCollector.visit(cu, methodNames);
         methodNames.forEach(n -> System.out.println("Method Name Collected: " + n));
-    }
 
-    private static class MethodNamePrinter extends VoidVisitorAdapter<Void> {
-
-        @Override
-        public void visit(MethodDeclaration md, Void arg) {
-            super.visit(md, arg);
-            System.out.println("Method Name Printed: " + md.getName());
-        }
+        List<String> conditionals = new ArrayList<>();
+        VoidVisitor<List<String>> conditionalCollector = new ConditionalCollector();
+        System.out.println("TEST2");
+        conditionalCollector.visit(cu, conditionals);
+        System.out.println("TEST3");
+        conditionals.forEach(n -> System.out.println("Conditional Collected: " + n));
     }
 
     private static class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
@@ -39,6 +36,16 @@ public class Parser {
         public void visit(MethodDeclaration md, List<String> collector) {
             super.visit(md, collector);
             collector.add(md.getNameAsString());
+        }
+    }
+
+    private static class ConditionalCollector extends VoidVisitorAdapter<List<String>> {
+
+        @Override
+        public void visit(ConditionalExpr ce, List<String> collector) {
+            super.visit(ce, collector);
+            collector.add(ce.getCondition().toString());
+            System.out.println("TEST");
         }
     }
 }
