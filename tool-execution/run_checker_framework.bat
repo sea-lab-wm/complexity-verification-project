390314eb3413b4be4098b6fb5f95c71b64b137d4
Kobi Feldman <kobif2001@gmail.com>
2023-01-27 11:32:50 -0500

    delete some old files, rename parser.py

--- tool-execution/run_checker_framework.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-call gradlew build --rerun-tasks 2> data\checker_framework_output.txt
-::call java -jar build\libs\complexity-verification-project.jar
-pause
--- tool-execution/run_correlation_analysis.sh
+++ tool-execution/run_correlation_analysis.sh
@@ -2,6 +2,6 @@
 
 #cd ..
 source complexity_verification_project_venv/bin/activate
-./complexity_verification_project_venv/bin/python3 parser.py
+./complexity_verification_project_venv/bin/python3 warning_parser.py
 ./complexity_verification_project_venv/bin/python3 correlation.py
 echo "Correlation Analysis Complete. View results in data/correlation_analysis.xlsx"
