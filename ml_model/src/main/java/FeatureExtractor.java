649bd4c9ab375e95e8779f2f23976ed8316981f9
ngimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2023-02-13 15:48:51 -0500

    Add Custom Feature Extractor

---
+++ ml_model/src/main/java/FeatureExtractor.java
@@ -0,0 +1,76 @@
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.google.common.base.Strings;
+import java.io.File;
+import java.io.IOException;
+
+public class FeatureExtractor {
+
+
+    // List all classes in a project
+    public static void listClasses(File projectDir) {
+        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+            System.out.println(path);
+            System.out.println(Strings.repeat("=", path.length()));
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+                        super.visit(n, arg);
+                        System.out.println(" * " + n.getName());
+                    }
+                }.visit(StaticJavaParser.parse(file), null);
+                System.out.println(); // empty line
+            } catch (IOException e) {
+                new RuntimeException(e);
+            }
+        }).explore(projectDir);
+    }
+
+    // List all methods in a project
+    public static void listMethods(File projectDir) {
+        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+            System.out.println(path);
+            System.out.println(Strings.repeat("=", path.length()));
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+                        super.visit(n, arg);
+                        n.getMethods().forEach(m -> System.out.println(" * " + m.getDeclarationAsString()));
+                    }
+                }.visit(StaticJavaParser.parse(file), null);
+                System.out.println(); // empty line
+            } catch (IOException e) {
+                new RuntimeException(e);
+            }
+        }).explore(projectDir);
+    }
+
+    // list all if statements in a project
+    public static void listIfStatements(File projectDir) {
+        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+            System.out.println(path);
+            System.out.println(Strings.repeat("=", path.length()));
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+                        super.visit(n, arg);
+                        n.getMethods().forEach(m -> m.getBody().ifPresent(b -> b.getStatements().forEach(s -> {
+                            if (s.isIfStmt()) {
+                                System.out.println(" * " + s);
+                            }
+                        })));
+                    }
+                }.visit(StaticJavaParser.parse(file), null);
+                System.out.println(); // empty line
+            } catch (IOException e) {
+                new RuntimeException(e);
+            }
+        }).explore(projectDir);
+    }
+    
+
+}
---
+++ ml_model/src/main/java/Parser_New.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class Parser_New {
+    private static final String FILE_PATH = "simple-datasets/src/main/java/cog_complexity_validation_datasets/One";
+    public static void main(String[] args) {
+        File projectDir = new File(FILE_PATH);
+        FeatureExtractor.listIfStatements(projectDir);
+    }
+}
