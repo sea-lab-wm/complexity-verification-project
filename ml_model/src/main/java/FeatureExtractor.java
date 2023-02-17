3cd7a1af62f393e15fdccc6e773eedc4ae4bb085
ngimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2023-02-17 18:56:09 -0500

    Add Java Parser feature extractor

--- ml_model/src/main/java/FeatureExtractor.java
+++ ml_model/src/main/java/FeatureExtractor.java
@@ -1,142 +1,107 @@
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.ForStmt;
-import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.WhileStmt;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-import com.google.common.base.Strings;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class FeatureExtractor {
 
+    private static Map<String, Object> features = null;
 
-    // List all classes in a project
-    public static void listClasses(File projectDir) {
-        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
-            System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
-            try {
-                new VoidVisitorAdapter<Object>() {
-                    @Override
-                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-                        super.visit(n, arg);
-                        System.out.println(" * " + n.getName());
+    public static class FeatureVisitor extends VoidVisitorAdapter<Void> {
+
+        // keep all the features
+        Map<String,Object> features = new HashMap<>();
+
+        // feature 1: #parameters
+        private  int numOfParameters = 0;
+
+        // feature 2: #if statements
+        Set<String> ifStatements = new HashSet<>();
+
+        // feature 3: #loops
+        Set<String> loops = new HashSet<>();
+
+        @Override
+        public void visit(MethodDeclaration md, Void arg){
+            super.visit(md, arg);
+            if (md.getBody().isPresent()) {
+
+                // get #parameters of method
+                numOfParameters = md.getParameters().size();
+
+                md.getBody().get().getStatements().forEach(s -> {
+                    if (s.isIfStmt()) {
+                        visit(s.asIfStmt(), arg);
                     }
-                }.visit(StaticJavaParser.parse(file), null);
-                System.out.println(); // empty line
-            } catch (IOException e) {
-                new RuntimeException(e);
+                    if (s.isForStmt()) {
+                        visit(s.asForStmt(), arg);
+                    }
+                    if (s.isWhileStmt()) {
+                        visit(s.asWhileStmt(), arg);
+                    }
+                });
             }
-        }).explore(projectDir);
+        }
+
+        @Override
+        public void visit(IfStmt ifStmt, Void arg) {
+            super.visit(ifStmt, arg);
+            ifStatements.add(ifStmt.toString());
+        }
+
+        @Override
+        public void visit(ForStmt forStmt, Void arg) {
+            super.visit(forStmt, arg);
+            loops.add(forStmt.toString());
+        }
+
+        @Override
+        public void visit(WhileStmt whileStmt, Void arg) {
+            super.visit(whileStmt, arg);
+            loops.add(whileStmt.toString());
+        }
+
+        public Map<String,Object> getFeatures() {
+            features.put("#parameters", numOfParameters);
+            features.put("ifStmts", ifStatements);
+            features.put("loops", loops);
+            return features;
+        }
     }
 
-    // List all methods in a project
-    public static void listMethods(File projectDir) {
+    public static void main(String[] args) {
+        FeatureVisitor featureVisitor = new FeatureVisitor();
+
+        String FILE_PATH = "simple-datasets/src/main/java/cog_complexity_validation_datasets/One";
+        File projectDir = new File(FILE_PATH);
+
+
         new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
-            System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
+            CompilationUnit cu = null;
             try {
-                new VoidVisitorAdapter<Object>() {
-                    @Override
-                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-                        super.visit(n, arg);
-                        n.getMethods().forEach(m -> System.out.println(" * " + m.getDeclarationAsString()));
-                    }
-                }.visit(StaticJavaParser.parse(file), null);
-                System.out.println(); // empty line
-            } catch (IOException e) {
-                new RuntimeException(e);
+                cu = StaticJavaParser.parse(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
+            featureVisitor.visit(cu,null);
+
+            // Uncomment If need to get the  individual features list
+            // features = featureVisitor.getFeatures();
+
         }).explore(projectDir);
-    }
 
-    // list all if statements in a project
-    public static void listIfStatements(File projectDir) {
-        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
-            System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
-            try {
-                new VoidVisitorAdapter<Object>() {
-                    @Override
-                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-                        super.visit(n, arg);
-                        n.getMethods().forEach(m -> m.getBody().ifPresent(b -> b.getStatements().forEach(s -> {
-                            if (s.isIfStmt()) {
-                                System.out.println(" * " + s);
-                            }
-                        })));
-                    }
-                }.visit(StaticJavaParser.parse(file), null);
-                System.out.println(); // empty line
-            } catch (IOException e) {
-                new RuntimeException(e);
-            }
-        }).explore(projectDir);
-    }
-    
-    // list all parameters in a project
-    public static void listParameters(File projectDir) {
-        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
-            System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
-            try {
-                new VoidVisitorAdapter<Object>() {
-                    @Override
-                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-                        super.visit(n, arg);
-                        n.getMethods().forEach(m -> m.getParameters().forEach(s -> {
-                            System.out.println(" * " + s);
-                        }));
-                    }
-                }.visit(StaticJavaParser.parse(file), null);
-                System.out.println(); // empty line
-            } catch (IOException e) {
-                new RuntimeException(e);
-            }
-        }).explore(projectDir);    
-    }
-
-    // list all loops in a project
-    public static void listLoopStatements(File projectDir) {
-        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
-            System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
-            try {
-                new VoidVisitorAdapter<Object>() {
-                    @Override
-                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-                        super.visit(n, arg);
-                        
-                        n.getMethods().forEach(m -> m.getBody().ifPresent(b -> b.getStatements().forEach(s -> {
-                            getAllNestedLoops(s);
-                        })));
-                    }
-
-                    private void getAllNestedLoops(Statement s) {
-                        if (s.isForStmt()) {
-                            System.out.println(" * " + s);
-                            getAllNestedLoops(s.asForStmt().getBody());
-                        }
-
-                        if (s.isForEachStmt()) {
-                            System.out.println(" * " + s);
-                            getAllNestedLoops(s.asForEachStmt().getBody());
-                        }
-
-                        if (s.isWhileStmt()) {
-                            System.out.println(" * " + s);
-                            getAllNestedLoops(s.asWhileStmt().getBody());
-                        }
-
-                        System.out.println(" ??? " + s);
-                    }
-                }.visit(StaticJavaParser.parse(file), null);
-                System.out.println(); // empty line
-            } catch (IOException e) {
-                new RuntimeException(e);
-            }
-        }).explore(projectDir);
+        System.out.println("Number of if statements: "+featureVisitor.ifStatements.size());
+        System.out.println("Number of loops: "+featureVisitor.loops.size());
     }
 
 }
