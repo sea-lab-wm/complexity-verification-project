d6cbc8b92d6a7c09b41e2efd55a4e260933538ff
Nadeeshan De Silva <gimhanadesilva.15@cse.mrt.ac.lk>
2023-06-15 11:55:56 +0530

    add lombok gradle dependency

--- ml_model/src/main/java/edu/wm/sealab/featureextraction/FeatureVisitor.java
+++ ml_model/src/main/java/edu/wm/sealab/featureextraction/FeatureVisitor.java
@@ -7,64 +7,60 @@ import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.WhileStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-
 /**
  * This class to extract features from a java snippet
- * Input : Java file with a Single Method (Note: if want to use all the features)
+ * Input : Java file with a Single Method (Note: if want to use all the
+ * features)
  * Output: Features of the java file
- * */ 
-// public class FeatureExtractor {
+ */
+public class FeatureVisitor extends VoidVisitorAdapter<Void> {
 
-    public class FeatureVisitor extends VoidVisitorAdapter<Void> {
+    // features of a java snippet
+    private Features features = new Features();
 
-        // features of a java snippet
-        private Features features = new Features();
-
-        /**
-         * This method to compute #parameters of a java method
-         */
-        @Override
-        public void visit(MethodDeclaration md, Void arg){
-            super.visit(md, arg);
-            if (md.getBody().isPresent()) {
-                // set #parameters of method
-                features.setNumOfParameters(md.getParameters().size());
-            }
-        }
-
-        /**
-         * This method to compute #if statements of a java method
-         */
-        @Override
-        public void visit(IfStmt ifStmt, Void arg) {
-            super.visit(ifStmt, arg);
-            features.setNumOfIfStatements(features.getNumOfIfStatements() + 1);
-        }
-
-        /**
-         * This method to compute #for loops of a java method
-         */
-        @Override
-        public void visit(ForStmt forStmt, Void arg) {
-            super.visit(forStmt, arg);
-            features.setNumOfLoops(features.getNumOfLoops() + 1);
-        }
-
-        /**
-         * This method to compute # while loops of a java method
-         */
-        @Override
-        public void visit(WhileStmt whileStmt, Void arg) {
-            super.visit(whileStmt, arg);
-            features.setNumOfLoops(features.getNumOfLoops() + 1);
-        }
-
-        /**
-         * This method to get the computed features
-         */
-        public Features getFeatures() {
-            return features;
+    /**
+     * This method to compute #parameters of a java method
+     */
+    @Override
+    public void visit(MethodDeclaration md, Void arg) {
+        super.visit(md, arg);
+        if (md.getBody().isPresent()) {
+            // set #parameters of method
+            features.setNumOfParameters(md.getParameters().size());
         }
     }
 
-// }
+    /**
+     * This method to compute #if statements of a java method
+     */
+    @Override
+    public void visit(IfStmt ifStmt, Void arg) {
+        super.visit(ifStmt, arg);
+        features.setNumOfIfStatements(features.getNumOfIfStatements() + 1);
+    }
+
+    /**
+     * This method to compute #for loops of a java method
+     */
+    @Override
+    public void visit(ForStmt forStmt, Void arg) {
+        super.visit(forStmt, arg);
+        features.setNumOfLoops(features.getNumOfLoops() + 1);
+    }
+
+    /**
+     * This method to compute # while loops of a java method
+     */
+    @Override
+    public void visit(WhileStmt whileStmt, Void arg) {
+        super.visit(whileStmt, arg);
+        features.setNumOfLoops(features.getNumOfLoops() + 1);
+    }
+
+    /**
+     * This method to get the computed features
+     */
+    public Features getFeatures() {
+        return features;
+    }
+}
