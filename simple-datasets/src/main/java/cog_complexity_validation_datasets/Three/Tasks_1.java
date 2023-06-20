8fa98195aa26265a70feca05385dc29a2edee671
Martin Kellogg <martin.kellogg@njit.edu>
2023-06-20 09:51:34 -0400

    avoid changing line numbers, so that we don't need to rerun openJml

--- simple-datasets/src/main/java/cog_complexity_validation_datasets/Three/Tasks_1.java
+++ simple-datasets/src/main/java/cog_complexity_validation_datasets/Three/Tasks_1.java
@@ -619,8 +619,7 @@ public class Tasks_1 {
      *
      * @deprecated
      */
-    @Deprecated
-    public void Date(int daysSince1970) { // return type void added to allow compilation
+    @Deprecated public void Date(int daysSince1970) { // return type void added to allow compilation
 
         long l = (long) daysSince1970 * 24 * 60 * 60 * 1000;
         java.util.Date d = new java.util.Date(l);
