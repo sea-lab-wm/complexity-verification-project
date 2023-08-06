8a99ff92cb2f0b47d5355533ea5daeadfbb180d3
Nadeeshan De Silva <gimhanadesilva.15@cse.mrt.ac.lk>
2023-08-05 20:43:55 -0400

    fix formating

--- ml_model/src/main/java/edu/wm/sealab/featureextraction/SnippetSplitter.java
+++ ml_model/src/main/java/edu/wm/sealab/featureextraction/SnippetSplitter.java
@@ -6,7 +6,6 @@ import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.comments.LineComment;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -173,7 +172,7 @@ public class SnippetSplitter {
 
         // Surround the method with a dummy class
         // String methodString = method.toString();
-        String methodString = LexicalPreservingPrinter.print(method);// get the raw method string
+        String methodString = LexicalPreservingPrinter.print(method); // get the raw method string
         String fullString =
             "package snippet_splitter_out.ds_"
                 + dataset
@@ -225,7 +224,7 @@ public class SnippetSplitter {
     SnippetSplitter ss =
         new SnippetSplitter(
             "ml_model/src/main/resources/manually_created_snippets/",
-            //"ml_model/src/main/resources/snippet_splitter_out/");
+            // "ml_model/src/main/resources/snippet_splitter_out/");
             "ml_model/src/main/resources/raw_snippet_splitter_out/"); // raw method output
     ss.run(
         new File("simple-datasets/src/main/java/cog_complexity_validation_datasets/One/"),
