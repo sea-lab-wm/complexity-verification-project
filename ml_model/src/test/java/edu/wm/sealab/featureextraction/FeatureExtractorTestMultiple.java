689a55f749299b729c52c14bcb19184b4f5d4699
dwigh <paradouble10@gmail.com>
2024-05-27 11:46:17 -0400

    Testing fix (statements & lines of code)

--- ml_model/src/test/java/edu/wm/sealab/featureextraction/FeatureExtractorTestMultiple.java
+++ ml_model/src/test/java/edu/wm/sealab/featureextraction/FeatureExtractorTestMultiple.java
@@ -38,14 +38,14 @@ public class FeatureExtractorTestMultiple {
   final int MAX_INDENTATION_LENGTH_1 = 20;
   final double AVG_INDENTATION_LENGTH_1 = 10.101694915254237;
   //line length avg and max
-  final int MAX_LINE_LENGTH_1 = 65;
-  final double AVG_LINE_LENGTH_1 = 24.847457627118644;
+  final int MAX_LINE_LENGTH_1 = 66;
+  final double AVG_LINE_LENGTH_1 = 25.847457627118644;
   //blank lines avg
   final double AVG_BLANK_LINES_1 = 0.03389830508474576;
   final int NUM_OF_ASSIGNMENT_EXPRESSIONS_1 = 4;
   final int NUM_OF_NUMBERS_1 = 24;
   final int MAX_NUMBERS_1 = 2;
-  final int NUM_OF_STATEMENTS_1 = 50;
+  final int NUM_OF_STATEMENTS_1 = 33;
 
 
   final int NUM_OF_LOOP_STATEMENTS_2 = 6;
@@ -59,7 +59,7 @@ public class FeatureExtractorTestMultiple {
   final int NUM_OF_ASSIGNMENT_EXPRESSIONS_2 = 5;
   final int NUM_OF_NUMBERS_2 = 16;
   final int MAX_NUMBERS_2 = 2;
-  final int NUM_OF_STATEMENTS_2 = 38;
+  final int NUM_OF_STATEMENTS_2 = 26;
 
   final double loc_2 = 48;
   final int NUM_OF_PARANTHESIS_2 = 40;
@@ -74,8 +74,8 @@ public class FeatureExtractorTestMultiple {
   final int MAX_INDENTATION_LENGTH_2 = 16;
   final double AVG_INDENTATION_LENGTH_2 = 9.833333333333334;
   //line length avg and max
-  final int MAX_LINE_LENGTH_2 = 51;
-  final double AVG_LINE_LENGTH_2 = 23.854166666666668;
+  final int MAX_LINE_LENGTH_2 = 52;
+  final double AVG_LINE_LENGTH_2 = 24.854166666666668;
   //blank lines avg
   final double AVG_BLANK_LINES_2 = 0.041666666666666664;
 
