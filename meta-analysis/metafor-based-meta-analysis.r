2d559cb2434494e0c07c6ee5b8c957b3616d0819
ngimhana <kgdesilva@wm.edu>
2026-02-12 22:28:47 -0500

    add meta corr results for dataset63

--- meta-analysis/metafor-based-meta-analysis.r
+++ meta-analysis/metafor-based-meta-analysis.r
@@ -12,7 +12,7 @@ rm(list = ls())
 
 #TODO: set the base path to the project
 #base_path = "~/Research/complexity-verification/complexity-verification-project/"
-base_path = "/Users/nadeeshan/Desktop/TOSEM/complexity-verification-project/"
+base_path = "/Users/nadeeshan/Desktop/TOSEM/2026-02-12/complexity-verification-project/"
 
 # Load libraries
 library(readxl)
